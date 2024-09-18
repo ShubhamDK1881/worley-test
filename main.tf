9cb90978e6ac3dc1912cf1605cb41c254a881e01
ShubhamDK1881 <163509278+ShubhamDK1881@users.noreply.github.com>
2024-09-19 01:00:40 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,10 +1,8 @@
 module "vpc" {
   source = "./modules/vpc"
   region = var.region
-  log_destination = module.s3.bucket_name_vpc_arn
   vpc_cidr = var.vpc_cidr
   vpc_name = var.vpc_name
-  internet_gateway_name = var.internet_gateway_name
   public_subnet1_cidr = var.public_subnet1_cidr
   public_subnet2_cidr = var.public_subnet2_cidr
   private_subnet1_cidr = var.private_subnet1_cidr
