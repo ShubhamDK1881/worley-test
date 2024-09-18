fc5149cd827dce3f4c02399831cea92dfc02c0b9
ShubhamDK1881 <163509278+ShubhamDK1881@users.noreply.github.com>
2024-09-19 00:14:45 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -7,6 +7,6 @@ terraform {
   backend "s3" {
     bucket = "terraform-be-worley-test-states"
     key = "terraform-be-worley-test-state/terraform.tfstate"
-    region = ap-south-1
+    region = "ap-south-1"
   }
 }
