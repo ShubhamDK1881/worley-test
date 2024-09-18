912bedafc09bc2a09edd1c2f0e8c6318c7916021
ShubhamDK1881 <163509278+ShubhamDK1881@users.noreply.github.com>
2024-09-19 00:54:53 +0530

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,19 +1,9 @@
-output "bucket_name" {
-  description = "The name of the S3 bucket"
-  value       = module.s3.bucket_name
+output "vpc_id" {
+  description = "The VPC ID"
+  value = module.vpc.id
 }
 
 output "vpc_arn" {
   description = "The VPC ID"
-  value = module.vpc.vpc_arn
-}
-
-output "DBClusterEndpoint" {
-  description = "The endpoint of the Aurora DB cluster"
-  value       = module.rds.DBClusterEndpoint
-}
-
-output "DBClusterReadEndpoint" {
-  description = "The read-only endpoint of the Aurora DB cluster"
-  value       = module.rds.DBClusterReadEndpoint
+  value = module.vpc.arn
 }
