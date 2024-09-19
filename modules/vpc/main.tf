1f84d089013b948dcdad7fd9f30d24d5be27ad56
ShubhamDK1881 <163509278+ShubhamDK1881@users.noreply.github.com>
2024-09-19 17:10:30 +0530

    Update main.tf

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -112,3 +112,29 @@ resource "aws_vpc_endpoint" "s3-vpc-endpoint" {
     Name = "worley-s3-vpc-endpoint"
   }
 }
+
+resource "aws_security_group" "db_security_group" {
+  name        = "db_security_group"
+  vpc_id      = aws_vpc.vpc.id
+
+  ingress {
+    from_port   = 5432
+    to_port     = 5432
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 0
+    to_port     = 65535
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
