67bc0fd604332858730a3b04649e21ade1e86005
ShubhamDK1881 <163509278+ShubhamDK1881@users.noreply.github.com>
2024-09-20 12:20:29 +0530

    Update output.tf

--- modules/vpc/output.tf
+++ modules/vpc/output.tf
@@ -7,13 +7,3 @@ output "vpc_arn" {
   description = "The VPC ID"
   value = aws_vpc.vpc.arn
 }
-
-output "private_subnet1_id" {
-  description = "The VPC ID"
-  value = aws_vpc.vpc.private_subnet1.id
-}
-
-output "private_subnet2_id" {
-  description = "The VPC ID"
-  value = aws_vpc.vpc.private_subnet2.id
-}
