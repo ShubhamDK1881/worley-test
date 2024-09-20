a130ab798c365b32c1c6c629db3f04818b4362f1
ShubhamDK1881 <163509278+ShubhamDK1881@users.noreply.github.com>
2024-09-20 12:28:26 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,11 @@
 module "vpc" {
   source = "./modules/vpc"
+  name = var.name
   region = var.region
+  log_destination = module.s3.bucket_name_vpc_arn
   vpc_cidr = var.vpc_cidr
   vpc_name = var.vpc_name
+  internet_gateway_name = var.internet_gateway_name
   public_subnet1_cidr = var.public_subnet1_cidr
   public_subnet2_cidr = var.public_subnet2_cidr
   private_subnet1_cidr = var.private_subnet1_cidr
@@ -13,18 +16,18 @@ module "vpc" {
   availability_zone2 = var.availability_zone2
 }
 
-module "rds" {
+
+/*module "rds" {
   source = "./modules/rds"
-  DBClusterName = var.DBClusterName
-  DBEngineVersion     = var.DBEngineVersion
-  DBBackupRetentionPeriod = var.DBBackupRetentionPeriod
-  Backupwindow = var.Backupwindow
-  Maintenancewindow = var.Maintenancewindow
-  DBName = var.DBName
-  DBInstnaceName = var.DBInstnaceName
-  InstanceType          = var.InstanceType
-  MonitoringInterval     = var.MonitoringInterval
-  PerformanceInsightsRetentionPeriod = var.PerformanceInsightsRetentionPeriod
-  DBEngineName = var.DBEngineName
-  vpc_name = var.vpc_name
-}
+  db_name = var.db_name
+  db_secret = var.db_secret
+  db_cluster_name = var.db_cluster_name
+  db_engine_name = var.db_engine_name
+  db_engine_version = var.db_engine_version
+  db_instance_name = var.db_instance_name
+  db_backup_retention_period = var.db_backup_retention_period
+  instance_type = var.instance_type
+  db_instnace_name = var.db_instance_name
+  performance_insights_retention_period = var.performance_insights_retention_period
+  monitoring_interval = var.monitoring_interval
+}*/
