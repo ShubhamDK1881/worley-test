4cf765db5ada15eb4a29439566134f6638a59883
ShubhamDK1881 <163509278+ShubhamDK1881@users.noreply.github.com>
2024-09-20 12:32:42 +0530

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,1 +1,14 @@
+output "vpc_arn" {
+  description = "The VPC ID"
+  value = module.vpc.vpc_arn
+}
 
+output "DBClusterReadEndpoint" {
+  description = "The read-only endpoint of the Aurora DB cluster"
+  value       = module.rds.DBClusterReadEndpoint
+}
+
+output "vpc_id" {
+  description = "The VPC ID"
+  value = module.vpc.vpc.id
+}
